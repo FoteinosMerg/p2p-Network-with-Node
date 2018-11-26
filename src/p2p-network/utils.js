15e5d369e34bd25d430ca2e588a50163bc18918b
FoteinosMerg <foteinosmerg@gmail.com>
2018-11-26 01:06:22 +0100

    Remove callback with false

--- src/p2p-network/utils.js
+++ src/p2p-network/utils.js
@@ -29,8 +29,8 @@ function extract_ADDRESS_and_PORT(URL) {
 
 function checkActivity(URL, callback) {
   /* Checks if the given URL is currently run by some remote server by ordering
-  a testing-server to listen to it. If yes, then handles this URL calling the
-  callback with true; otherwise, it handles it calling the callback with false */
+  a testing-server to listen to it. If yes, then handles applies callback to
+  the given URL */
 
   // Testing-server configuration
   const server = net.createServer();
@@ -38,7 +38,6 @@ function checkActivity(URL, callback) {
     if (err.code === "EADDRINUSE") callback(URL); //, true);
   });
   server.once("listening", () => {
-    //callback(URL, false);
     server.close();
   });
 
